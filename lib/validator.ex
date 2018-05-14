c3eedb34f2f209c3fe8b70ba309f53085e207001
olek-sii <oleksii.al@gmail.com>
2018-05-14 12:12:35 +0200

    refator validator

--- lib/validator.ex
+++ lib/validator.ex
@@ -1,21 +1,10 @@
 defmodule Validator do
-  @trasitisions_table [
-    start:  [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :minus],
-    number: [op: :op,     number: :number,  left: :error, right: :right,  dot: :dot,    minus: :op],
-    op:     [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :error],
-    left:   [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :minus],
-    right:  [op: :op,     number: :error,   left: :error, right: :right,  dot: :error,  minus: :op],
-    dot:    [op: :error,  number: :float,   left: :error, right: :error,  dot: :error,  minus: :error],
-    float:  [op: :op,     number: :float,   left: :error, right: :right,  dot: :error,  minus: :op],
-    minus:  [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :error]
-  ]
-
   def validate(input) do
     {state, stack} = loop(:start, transform(String.graphemes(input)), [0])
     (state == :number or state == :right or state == :float) and stack == [0]
   end
 
-  def transform(grapfemes) do
+  defp transform(grapfemes) do
     Enum.map(grapfemes, fn g ->
       cond do
         g >= "0" and g <= "9" -> :number
@@ -28,13 +17,25 @@ defmodule Validator do
     end)
   end
 
-  defp get(state, sym), do: @trasitisions_table |> Keyword.get(state) |> Keyword.get(sym)
+  defp next_state(state, sym) do
+    trasitisions_table = [
+      start:  [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :minus],
+      number: [op: :op,     number: :number,  left: :error, right: :right,  dot: :dot,    minus: :op],
+      op:     [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :error],
+      left:   [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :minus],
+      right:  [op: :op,     number: :error,   left: :error, right: :right,  dot: :error,  minus: :op],
+      dot:    [op: :error,  number: :float,   left: :error, right: :error,  dot: :error,  minus: :error],
+      float:  [op: :op,     number: :float,   left: :error, right: :right,  dot: :error,  minus: :op],
+      minus:  [op: :error,  number: :number,  left: :left,  right: :error,  dot: :error,  minus: :error]
+    ]
+    trasitisions_table |> Keyword.get(state) |> Keyword.get(sym)
+  end
 
-  def loop(:error, _, _), do: {:error, []}
-  def loop(q, [:left|[]], stack), do: {get(q, :left), [1 | stack]}
-  def loop(q, [:left|t], stack), do: loop(get(q, :left), t, [1 | stack])
-  def loop(q, [:right|[]], [_|stack]), do: {get(q, :right), stack}
-  def loop(q, [:right|t], [_|stack]), do: loop(get(q, :right), t, stack)
-  def loop(q, [h|[]], stack), do: { get(q, h), stack}
-  def loop(q, [h|t], stack), do: loop(get(q, h), t, stack)
+  defp loop(:error, _, _), do: {:error, []}
+  defp loop(q, [:left|[]], stack), do: {next_state(q, :left), [1 | stack]}
+  defp loop(q, [:left|t], stack), do: loop(next_state(q, :left), t, [1 | stack])
+  defp loop(q, [:right|[]], [_|stack]), do: {next_state(q, :right), stack}
+  defp loop(q, [:right|t], [_|stack]), do: loop(next_state(q, :right), t, stack)
+  defp loop(q, [h|[]], stack), do: { next_state(q, h), stack}
+  defp loop(q, [h|t], stack), do: loop(next_state(q, h), t, stack)
 end
